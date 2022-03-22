920bea5807897d73b6ba50c044af28db62cd3ea0
lyudmila50 <81586606+lyudmila50@users.noreply.github.com>
2022-03-22 11:11:10 -0300

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+         
+        System.out.println("Agenda de " + nombre);
+        
+
+        Scanner sca = new Scanner(System.in);
+        final String nombre  = sca.nextLine();
+        System.out.println("nombre:"+ nombre);
+        final String numero =  sca.nextLine();
+        System.out.println("numero:"+ numero);
+        final String email =  sca.nextLine();
+        System.out.println("emeil:"+ email);
+
+        System.out.println("----------***************---------------");
+        System.out.println("               MENU                       ");
+        System.out.println(" 1- Ingrese un nuevo contacto");
+        System.out.println(" 2- Consutar un conacto existe");
+        System.out.println("3- ")
+
+
+    
+       
+       
+       
+
+
+
+    }
+}
